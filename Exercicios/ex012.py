8468a31bfe13397b6e58b9e9c18dbda91bf7a59e
Ana Luiza <analuizamoraespro@gmail.com>
2022-08-06 12:50:08 -0300

    Segundo commit

---
+++ Exercicios/ex012.py
@@ -0,0 +1,7 @@
+""" Faça um algoritmo que leia o preço de um produto
+e mostre seu novo preço com 5% de desconto"""
+
+produto = float(input('Digite o preço do seu produto: R$'))
+desconto = produto*(5/100)
+produtoComDesconto = produto-desconto
+print(f'O desconto de 5% em cima do seu produto é de R${desconto}\nO valor com o desconto fica: R${produtoComDesconto}')
---
+++ Exercicios/ex013.py
@@ -0,0 +1,11 @@
+"""Faça um algoritmo que leia o salário de um funcionario
+e mostre seu novo salario com 15% de aumento"""
+
+salario = float(input('Digite seu salário atual: R$'))
+aumento = salario*(15/100)
+salarioComAumento = salario+aumento
+print(f'Seu salário de R${salario}, com um aumento de 15% fica: R${round( salarioComAumento, 2 )}')
+
+"""Utilizei o {round( salarioComAumento, 2 )} 
+para colocar somente duas casas após a vírgula 
+no resultado final"""
