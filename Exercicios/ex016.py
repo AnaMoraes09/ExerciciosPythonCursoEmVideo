7496b389ed562f977e0da81e9389be8c633ec873
Ana Luiza <analuizamoraespro@gmail.com>
2022-08-09 16:20:27 -0300

    Terceirp commit

---
+++ Exercicios/ex016.py
@@ -0,0 +1,3 @@
+"""Ex016 Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira."""
+num = float(input('Digite um valor: '))
+print(f'Seu valor arredondado é: {round(num)}')
---
+++ Exercicios/ex020.py
@@ -0,0 +1,11 @@
+"""O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos.
+Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada."""
+import random
+
+aluno1 = str(input('Digite o nome do primeiro aluno: '))
+aluno2 = str(input('Digite o nome do segundo aluno: '))
+aluno3 = str(input('Digite o nome do terceiro aluno: '))
+aluno4 = str(input('Digite o nome do quarto aluno: '))
+listaDeAluno = [aluno1, aluno2, aluno3, aluno4]
+random.shuffle(listaDeAluno)
+print(f'\nA ordem dos alunos sorteados é: {listaDeAluno}')
